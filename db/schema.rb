659bcb367052174c6936d833f1664fbac9f0d69e
ryosakamoto39 <ryosakamoto39@gmail.com>
2020-09-03 15:35:21 +0900

    サインアップにプロフィール画像追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_03_044306) do
+ActiveRecord::Schema.define(version: 2020_09_03_061346) do
 
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 2020_09_03_044306) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
