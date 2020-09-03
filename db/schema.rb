0df0c603031cf87d906f983463a6c087dce6d88b
ryosakamoto39 <ryosakamoto39@gmail.com>
2020-09-03 13:45:15 +0900

    usersテーブルにnameカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_03_023653) do
+ActiveRecord::Schema.define(version: 2020_09_03_044306) do
 
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 2020_09_03_023653) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
