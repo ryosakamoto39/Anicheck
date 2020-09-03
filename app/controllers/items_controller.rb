1b728d94765eadd1bf6dc584fcc5dba21e0bdb80
ryosakamoto39 <ryosakamoto39@gmail.com>
2020-09-03 18:18:54 +0900

    ルーティングの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,6 @@
 class ItemsController < ApplicationController
-  before_action user_admin, only: [:new]
 
   def new
   end
-
-    private
-      def user_admin
-         @users = User.all
-         if current_user.admin == false
-             redirect_to root_path
-         else
-             render action: "new"
-         end
-      end
 
 end
