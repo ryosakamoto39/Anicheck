6fa5504500734b9d3d44a07760a31f384167b3f3
ryosakamoto39 <ryosakamoto39@gmail.com>
2020-09-03 17:36:41 +0900

    ルーティング一覧作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,18 @@
 class ItemsController < ApplicationController
+  before_action user_admin, only: [:new]
+
+  def new
+    @item = Item.new
+  end
+
+    private
+      def user_admin
+         @users = User.all
+         if  current_user.admin == false
+             redirect_to root_path
+         else
+             render action: "new"
+         end
+      end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
   get '/' => 'home#top', as: 'root'
+  get 'search' => 'search#show', as: 'search'
   devise_for :users
-  get 'users/show'
-
+  resources :items, except: %i[index]
+  resources :reviews, except: %i[index show]
+  resources :manufacturers, only: %i[edit update destroy]
+  resources :categories, only: %i[edit update destroy]
+  resources :tags, only: %i[destroy]
+  resources :comments, except: %i[index]
+  resources :rooms, only: %i[index show create]
+  resources :relationships, only: %i[create destroy]
+  resources :comment_likes, only: %i[create destroy]
+  resources :review_likes, only: %i[create destroy]
+  resources :watched_items, only: %i[create destroy]
+  resources :want_to_watch_items, only: %i[create destroy]
+  resources :users, except: %i[index]
 end
