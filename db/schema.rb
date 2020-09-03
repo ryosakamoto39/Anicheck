34425fee1af35185320d943aeac7113bc9b39d6c
ryosakamoto39 <ryosakamoto39@gmail.com>
2020-09-03 16:52:50 +0900

    管理者ユーザー追加・管理者専用リンク作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_03_061346) do
+ActiveRecord::Schema.define(version: 2020_09_03_071849) do
 
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 2020_09_03_061346) do
     t.datetime "remember_created_at"
     t.string "name"
     t.string "image"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
