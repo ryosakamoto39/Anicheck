3340da12ebd2cfda975366bb03665ff627078828
ryosakamoto39 <ryosakamoto39@gmail.com>
2020-09-05 18:41:56 +0900

    作品登録ページ作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,29 @@
 class ItemsController < ApplicationController
 
   def new
+    @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      flash[:notice] = "作成しました"
+      #redirect_to "/items/#{@item.id}"
+      render 'new'
+    else
+      rollback
+      flash.now[:notice] = "作成に失敗しました"
+      render 'new'
+    end
+  end
+
+    private
+
+      def item_params
+        params.require(:item).permit(
+        :title, :story, :image
+      )
+      end
+
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 30 }, uniqueness: true
+  validates :story, presence: true, length: { maximum: 800 }
+
 end
