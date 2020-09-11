826466ba3936d43a952867a4958af151cd68df3e
ryosakamoto39 <ryosakamoto39@gmail.com>
2020-09-11 21:14:49 +0900

    wtwiのrspec更新

--- spec/models/want_to_watch_item_spec.rb
+++ spec/models/want_to_watch_item_spec.rb
@@ -8,5 +8,23 @@ RSpec.describe WantToWatchItem, type: :model do
     expect { FactoryBot.create(:want_to_watch_item) }.to change(WantToWatchItem.all, :count).by(1)
   end
 
+  describe "presence of user_id, item_id" do
+    it "is valid with a user_id and item_id" do
+      want_to_watch_item = WantToWatchItem.new(user_id: user.id, item_id: item.id)
+      expect(want_to_watch_item).to be_valid
+    end
+  end
+
+    it "is invalid without a user_id" do
+      want_to_watch_item = WantToWatchItem.new(user_id: nil, item_id: item.id)
+      want_to_watch_item.valid?
+      expect(want_to_watch_item.errors[:user_id]).to include("can't be blank")
+    end
+
+    it "is invalid without a item_id" do
+      want_to_watch_item = WantToWatchItem.new(user_id: user.id, item_id: nil)
+      want_to_watch_item.valid?
+      expect(want_to_watch_item.errors[:item_id]).to include("can't be blank")
+    end
 
 end
