2f746492b359fb57240ceed912e5fded82202377
ryosakamoto39 <ryosakamoto39@gmail.com>
2020-09-13 15:48:45 +0900

    comment_likes_controllerの記述

--- app/controllers/comment_likes_controller.rb
+++ app/controllers/comment_likes_controller.rb
@@ -1,2 +1,23 @@
 class CommentLikesController < ApplicationController
+
+  def create
+    @comment = Comment.find(params[:comment_id])
+    CommentLike.create(user_id: current_user.id, comment_id: @comment.id)
+    @count = params[:count].to_i + 1
+
+    respond_to do |format|
+        format.js
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    CommentLike.find_by(user_id: current_user.id, comment_id: params[:id]).destroy
+    @count = params[:count].to_i - 1
+
+    respond_to do |format|
+      format.js
+    end
+  end
+
 end
