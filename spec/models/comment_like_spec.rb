77990c16eadac60c446814ceb486cbc490c0a567
ryosakamoto39 <ryosakamoto39@gmail.com>
2020-09-13 15:43:12 +0900

    comment_likeのRspec追加

--- spec/models/comment_like_spec.rb
+++ spec/models/comment_like_spec.rb
@@ -8,4 +8,26 @@ RSpec.describe CommentLike, type: :model do
     expect { FactoryBot.create(:comment_like) }.to change(CommentLike.all, :count).by(1)
   end
 
+  describe "presence of user_id, comment_id" do
+    it "is valid with a user_id and comment_id" do
+      comment_like = CommentLike.new(user_id: user.id, comment_id: comment.id)
+      expect(comment_like).to be_valid
+    end
+
+    it "is invalid without a user_id" do
+      comment_like = CommentLike.new(user_id: nil, comment_id: comment.id)
+      comment_like.valid?
+      expect(comment_like.errors[:user_id]).to include("can't be blank")
+    end
+
+    it "is inavlid without a comment_id" do
+      comment_like = CommentLike.new(user_id: user.id, comment_id: nil)
+      comment_like.valid?
+      expect(comment_like.errors[:comment_id]).to include("can't be blank")
+    end
+
+
+
+  end
+
 end
