e8c009b0736ddec81e2ba4bd15c0995533758b05
ryosakamoto39 <ryosakamoto39@gmail.com>
2020-09-14 14:26:16 +0900

    フォロー機能の実装（非Ajax）

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,16 +2,16 @@ class RelationshipsController < ApplicationController
 
   def create
     @user = User.find(params[:user_id])
-    @current_user.follow(@user)
+    current_user.follow(@user)
     respond_to do |format|
       format.html { redirect_to @user }
       format.js
     end
   end
-    
+
   def destroy
     @user = User.find(params[:id])
-    @current_user.unfollow(@user)
+    current_user.unfollow(@user)
     respond_to do |format|
       format.html { redirect_to @user }
       format.js
