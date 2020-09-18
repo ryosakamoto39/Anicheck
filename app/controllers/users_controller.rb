c25f0d5f6f694558599e397e5be2a23a8ac54413
ryosakamoto39 <ryosakamoto39@gmail.com>
2020-09-18 11:43:22 +0900

    ユーザー画面のタブ情報表示を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,64 @@ class UsersController < ApplicationController
         @entry = Entry.new
       end
     end
-  end
 
+    @type = params[:type] || "review"
+
+    if @type == "review"
+      @results = @user.reviews.includes(:item)
+      respond_to do |format|
+        format.js
+        format.html
+      end
+      return
+    end
+
+    if @type == "want_to_watch_item"
+     item_ids = "SELECT item_id FROM want_to_watch_items WHERE user_id = :user_id"
+     @results = Item.where("id IN (#{item_ids})", user_id: @user.id)
+     respond_to do |format|
+       format.js
+       format.html
+     end
+     return
+   end
+
+    if @type == "watched_item"
+      item_ids = "SELECT item_id FROM watched_items WHERE user_id = :user_id"
+      @results = Item.where("id IN (#{item_ids})", user_id: @user.id)
+      respond_to do |format|
+        format.js
+        format.html
+      end
+      return
+    end
+
+    if @type == "liked_review"
+      review_ids = "SELECT review_id FROM review_likes WHERE user_id = :user_id"
+      @results = Review.where("id IN (#{review_ids})", user_id: @user.id).includes(:item)
+      respond_to do |format|
+        format.js
+        format.html
+      end
+      return
+    end
+
+    if @type == "following"
+      @results = @user.following
+      respond_to do |format|
+        format.js
+        format.html
+      end
+      return
+    end
+
+    return unless @type == "follower"
+
+    @results = @user.followers
+    respond_to do |format|
+      format.js
+      format.html
+    end
+  end
+  
 end
