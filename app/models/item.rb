65d5737515e3db4a35d3bd2c90134a371abb09ad
ryosakamoto39 <ryosakamoto39@gmail.com>
2020-09-24 10:54:39 +0900

    CSSの調整

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   validates :title, presence: true, length: { maximum: 30 }, uniqueness: true
-  validates :story, presence: true, length: { maximum: 800 }
+  validates :story, presence: true, length: { maximum: 300 }
   mount_uploader :image, ImageUploader
   has_many :reviews
   has_many :want_to_watch_items
