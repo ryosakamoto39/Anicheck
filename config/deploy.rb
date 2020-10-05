abdf4a790854e5dc2fb781aa8a9d2f7886f8b5f1
ryosakamoto39 <ryosakamoto39@gmail.com>
2020-10-05 20:51:45 +0900

    capistranoの設定ファイルを更新

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,6 +19,8 @@ set :deploy_to, '/var/www/Anicheck'
 # シンボリックリンクをはるフォルダ。
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
+set :linked_dirs, %w(public/default)
+
 # 保持するバージョンの個数
 set :keep_releases, 5
 
