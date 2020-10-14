481f26345593e9b261bec1ad52d08c396271e03a
ryosakamoto39 <ryosakamoto39@gmail.com>
2020-10-14 10:55:22 +0900

    S3のアクセスエラーを解消するため、設定ファイル変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,6 +14,7 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'anicheck-s3'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/anicheck-s3'
+    config.fog_public = false
     # iam_profile
     config.fog_credentials = {
       provider: 'AWS',
