99adc94750e075be7c6fc7f06a3367a18b659513
ryosakamoto39 <ryosakamoto39@gmail.com>
2020-10-17 21:14:26 +0900

    コードの微修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,9 +2,9 @@ class MessagesController < ApplicationController
   def create
     if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
       @message = Message.create(params.require(:message).permit(:user_id, :content, :room_id).merge(user_id: current_user.id))
-      @room=@message.room
-      @roommembernotme=Entry.where(room_id: @room.id).where.not(user_id: current_user.id)
-            @theid=@roommembernotme.find_by(room_id: @room.id)
+      @room = @message.room
+      @roommembernotme = Entry.where(room_id: @room.id).where.not(user_id: current_user.id)
+            @theid = @roommembernotme.find_by(room_id: @room.id)
             notification = current_user.active_notifications.new(
                 room_id: @room.id,
                 message_id: @message.id,
