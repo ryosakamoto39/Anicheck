661a06700c8dd01f57854920d53437991bc33cf9
ryosakamoto39 <ryosakamoto39@gmail.com>
2020-10-17 17:13:22 +0900

    notificationのデザインを編集

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -9,15 +9,15 @@ module NotificationsHelper
     case notification.action
     when 'follow'
       #aタグで通知を作成したユーザーshowのリンクを作成
-      tag.a(notification.visitor.name, href: user_path(@visitor)) + 'があなたをフォローしました'
+      tag.a(notification.visitor.name, href: user_path(@visitor)) + 'さんがあなたをフォローしました'
     when 'like'
-      tag.a(notification.visitor.name, href: user_path(@visitor)) + 'が' + tag.a('あなたのレビュー', href: review_path(notification.review_id)) + 'にいいねしました'
+      tag.a(notification.visitor.name, href: user_path(@visitor)) + 'が' + tag.a('あなたのレビュー', href: item_path(notification.review.item.id)) + 'にいいねしました'
     when 'comment' then
       #コメントの内容と投稿のタイトルを取得　
       @comment = Comment.find_by(id: @visitor_comment)
       @comment_content = @comment.content
       @review_title = @comment.review.item.title
-      tag.a(@visitor.name, href: user_path(@visitor)) + 'が' + tag.a("#{@review_title}", href: review_path(notification.review_id)) + 'にコメントしました'
+      tag.a(@visitor.name, href: user_path(@visitor)) + 'が' + tag.a("#{@review_title}", href: item_path(notification.review.item.id)) + 'にコメントしました'
     end
   end
 end
