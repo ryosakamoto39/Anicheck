dca7ee34b1fa5597b4fdb694a4f685308569cab5
ryosakamoto39 <ryosakamoto39@gmail.com>
2020-11-12 19:04:07 +0900

    テスト項目の表記を修正

--- spec/models/comment_like_spec.rb
+++ spec/models/comment_like_spec.rb
@@ -4,23 +4,23 @@ RSpec.describe CommentLike, type: :model do
   let(:user) { FactoryBot.create(:user) }
   let(:comment) { FactoryBot.create(:comment) }
 
-  it 'has a valid factory' do
+  it '有効なファクトリーを持つこと' do
     expect { FactoryBot.create(:comment_like) }.to change(CommentLike.all, :count).by(1)
   end
 
-  describe 'presence of user_id, comment_id' do
+  describe 'user_idとcomment_idが存在すること' do
     it 'is valid with a user_id and comment_id' do
       comment_like = CommentLike.new(user_id: user.id, comment_id: comment.id)
       expect(comment_like).to be_valid
     end
 
-    it 'is invalid without a user_id' do
+    it 'user_idが入力されていること' do
       comment_like = CommentLike.new(user_id: nil, comment_id: comment.id)
       comment_like.valid?
       expect(comment_like.errors[:user_id]).to include("を入力してください")
     end
 
-    it 'is inavlid without a comment_id' do
+    it 'comment_idが入力されていること' do
       comment_like = CommentLike.new(user_id: user.id, comment_id: nil)
       comment_like.valid?
       expect(comment_like.errors[:comment_id]).to include("を入力してください")
