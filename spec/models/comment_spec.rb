fc01b85a2761742471bbe4973e1c788b5b20d317
ryosakamoto39 <ryosakamoto39@gmail.com>
2020-11-14 20:18:32 +0900

    テスト項目の可読性向上

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -10,43 +10,43 @@ RSpec.describe Comment, type: :model do
   let(:self_comment) { FactoryBot.create(:comment, review: self_review, user:user) }
   let(:self_review) { FactoryBot.create(:review, user: user) }
 
-  it 'has a valid factory' do
+  it '有効なファクトリーを持つこと' do
     expect { FactoryBot.create(:comment) }.to change(Comment.all, :count).by(1)
   end
 
-  describe 'presence of user_id, review_id, content' do
-    it 'is valid with a user_id and review_id, content' do
+  describe 'user_idとreview_id、contentの存在性' do
+    it 'user_idとreview_id、contentが存在すること' do
       comment = Comment.new(user_id: user.id, review_id: review.id, content: 'test')
       expect(comment).to be_valid
     end
 
-    it 'is invalid without a user_id' do
+    it 'user_idが存在すること' do
       comment = Comment.new(user_id: nil, review_id: review.id, content: 'test')
       comment.valid?
       expect(comment.errors[:user_id]).to include("を入力してください")
     end
 
-    it 'is inavlid without a review_id' do
+    it 'review_idが存在すること' do
       comment = Comment.new(user_id: user.id, review_id: nil, content: 'test')
       comment.valid?
       expect(comment.errors[:review_id]).to include("を入力してください")
     end
 
-    it 'is inavlid without a content' do
+    it 'contentが存在すること' do
       comment = Comment.new(user_id: user.id, review_id: review.id, content: nil)
       comment.valid?
       expect(comment.errors[:content]).to include("を入力してください")
     end
   end
 
-  describe 'length of comment' do
-    it 'is invalid with a comment which has over 201 characters' do
+  describe 'commentの文字数について' do
+    it 'commentが201文字以上であること' do
       comment.content = 'あ' * 201
       comment.valid?
       expect(comment.errors[:content]).to include('は200文字以内で入力してください')
     end
 
-    it 'is valid with a comment which has 200 characters' do
+    it 'commentが200文字以内であること' do
       comment.content = 'あ' * 200
       comment.valid?
       expect(comment).to be_valid
