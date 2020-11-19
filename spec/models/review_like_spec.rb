0bc47486c32f7d25d245b126da262f2e28387e25
ryosakamoto39 <ryosakamoto39@gmail.com>
2020-11-19 20:51:29 +0900

    テスト項目の可読性を向上

--- spec/models/review_like_spec.rb
+++ spec/models/review_like_spec.rb
@@ -4,23 +4,23 @@ RSpec.describe ReviewLike, type: :model do
   let(:user) { FactoryBot.create(:user) }
   let(:review) { FactoryBot.create(:review) }
 
-  it 'has a valid factory' do
+  it '有効なファクトリーを持つこと' do
     expect { FactoryBot.create(:review_like) }.to change(ReviewLike.all, :count).by(1)
   end
 
-  describe 'presence of user_id, review_id' do
-    it 'is valid with a user_id and review_id' do
+  describe 'user_idとreview_idの存在性' do
+    it 'user_idとreview_idが存在すること' do
       review_like = ReviewLike.new(user_id: user.id, review_id: review.id)
       expect(review_like).to be_valid
     end
 
-    it 'is invalid without a user_id' do
+    it 'user_idがなければ無効であること' do
       review_like = ReviewLike.new(user_id: nil, review_id: review.id)
       review_like.valid?
       expect(review_like.errors[:user_id]).to include("を入力してください")
     end
 
-    it 'is inavlid without a review_id' do
+    it 'review_idがなければ無効であること' do
       review_like = ReviewLike.new(user_id: user.id, review_id: nil)
       review_like.valid?
       expect(review_like.errors[:review_id]).to include("を入力してください")
