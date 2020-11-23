6f4cfd587456a9fe04fea08412c50e1f4c4f4d33
ryosakamoto39 <ryosakamoto39@gmail.com>
2020-11-23 20:26:32 +0900

    テスト項目の可動性を向上

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Review, type: :model do
   let(:item) { FactoryBot.create(:item) }
   let(:review) { FactoryBot.create(:review) }
 
-  it 'has a valid factory' do
+  it '有効なファクトリーを持つこと' do
     expect { FactoryBot.create(:review) }.to change(Review.all, :count).by(1)
   end
 
