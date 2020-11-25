48ee7fdc015ab0f7c0795f2d6c64836b79b62907
ryosakamoto39 <ryosakamoto39@gmail.com>
2020-11-25 20:16:52 +0900

    テスト項目の可読性を向上

--- spec/models/want_to_watch_item_spec.rb
+++ spec/models/want_to_watch_item_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe WantToWatchItem, type: :model do
   let(:user) { FactoryBot.create(:user) }
   let(:item) { FactoryBot.create(:item) }
 
-  it 'has a valid factory' do
+  it '有効なファクトリーを持つこと' do
     expect { FactoryBot.create(:want_to_watch_item) }.to change(WantToWatchItem.all, :count).by(1)
   end
 
